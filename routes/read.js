1f8f083a226eb958bfe8bc81adfd67209fc45942
Dasha Lary <dasha4988@gmail.com>
2021-04-16 17:24:15 -0400

    added comments to read route

--- routes/read.js
+++ routes/read.js
@@ -2,14 +2,16 @@ const express = require("express");
 const router = express.Router();
 const db = require("../config/database");
 
+/* this query says to find a note with a matching id and display it to the user */
+
 router.get("/:id", async (req, res) => {
   const query = `
     SELECT * FROM Note
       WHERE id=$1;
       `;
-  const values = [req.params.id];
-  const { rows } = await db.query(query, values);
-  res.render("read", { data: rows[0] });
+  const values = [req.params.id]; // id param in the url will be the param for the SQL command
+  const { rows } = await db.query(query, values); // run the query
+  res.render("read", { data: rows[0] }); // render read.pug file and send the returned data from the SQL command to the page
 });
 
 module.exports = router;
